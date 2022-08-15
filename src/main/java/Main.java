e318eb727015e5a0720823beb972e834bc672c68
Oleh <oleh@Oleh.local>
2022-08-15 18:01:44 +0300

    started work in progress

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+    public static Logger logger = LogManager.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        logger.info("Message info");
+        logger.debug("Message debug");
+    }
+}
